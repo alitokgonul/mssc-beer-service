cff9ccedd6806b789e5dc23554d0dbcc64d11c49
tokgonul <Ali.Tokgonul@adesso.com.tr>
2020-01-28 11:03:34 +0300

    model created

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by at 1/28/2020
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
