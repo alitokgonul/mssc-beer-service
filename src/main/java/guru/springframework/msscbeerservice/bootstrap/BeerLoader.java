16d19f57e54d7e08be9004146a887e8a9035353b
tokgonul <Ali.Tokgonul@adesso.com.tr>
2020-01-28 13:47:48 +0300

    bootstrap added

---
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,50 @@
+package guru.springframework.msscbeerservice.bootstrap;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by at 1/28/2020
+ */
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    public void loadBeerObjects(){
+        if(beerRepository.count() == 0) {
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(33610000001L)
+                    .price(new BigDecimal("12.23"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(33613200001L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+    }
+}
